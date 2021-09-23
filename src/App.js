aa4932672da80a37f3a7ad8105c7c62ea8383787
Maruf Ahmed <74198957+lYlarufAhmed@users.noreply.github.com>
2021-09-23 12:29:00 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import {useCallback, useRef} from 'react'
 import './App.css';
+import {WaveSurfer, WaveForm} from "wavesurfer-react";
 
 function App() {
+  let url = 'https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3'
+  const wavesurferRef = useRef()
+  const play = useCallback(() => {
+          wavesurferRef.current.playPause();
+      }, []);
+  const handleWSMount = useCallback(
+          (waveSurfer) => {
+              wavesurferRef.current = waveSurfer;
+              if (wavesurferRef.current) {
+                  wavesurferRef.current.load(url);
+
+
+                  wavesurferRef.current.on("ready", () => {
+                      console.log("WaveSurfer is ready");
+                  });
+
+                  wavesurferRef.current.on('finish', () => {
+                      console.log('track End')
+                  })
+
+
+                  wavesurferRef.current.on("loading", data => {
+                      console.log("loading --> ", data);
+                  });
+
+
+                  if (window) {
+                      window.surferidze = wavesurferRef.current;
+                  }
+              }
+          },
+          [url]
+      );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <WaveSurfer onMount={handleWSMount}>
+                        <WaveForm waveColor={'f4f2f2'} cursorColor={'#00000000'} progressColor={'#EA1073'} height={50}
+                                  id={`waveform`} fillParent={false} responsive={true}
+                            //   pixelRatio={1}
+                            backend={'MediaElement'}
+                                  barHeight={3}
+                        >
+                        </WaveForm>
+                    </WaveSurfer>
+      <button onClick={play}>play/pause</button>
     </div>
   );
 }
