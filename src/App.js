a2ba552634a5960a64ce58234dabd8a79ec19d1a
Maruf Ahmed <74198957+lYlarufAhmed@users.noreply.github.com>
2021-09-23 13:21:58 +0600

    added recorder and 2 different audio component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import {useCallback, useRef} from 'react'
 import './App.css';
 import {WaveSurfer, WaveForm} from "wavesurfer-react";
+import {useReactMediaRecorder} from 'react-media-recorder'
 
 function App() {
-  let url = 'https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3'
+  // let url = 'https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3'
   const wavesurferRef = useRef()
+  // let [recording, setRecording] = useState(false)
+  // let [requireStop, setrequireStop] = useState(false)
+  const {
+      status,
+      startRecording,
+      stopRecording,
+      mediaBlobUrl,
+    } = useReactMediaRecorder({ audio: true });
   const play = useCallback(() => {
           wavesurferRef.current.playPause();
       }, []);
+  const handleToggleRecoring = () => {
+     if (status === 'recording')
+      stopRecording()
+     else
+      startRecording()
+   }
   const handleWSMount = useCallback(
           (waveSurfer) => {
               wavesurferRef.current = waveSurfer;
               if (wavesurferRef.current) {
-                  wavesurferRef.current.load(url);
+                  wavesurferRef.current.load(mediaBlobUrl);
 
 
                   wavesurferRef.current.on("ready", () => {
@@ -34,10 +49,13 @@ function App() {
                   }
               }
           },
-          [url]
+          [mediaBlobUrl]
       );
   return (
     <div className="App">
+      {
+        mediaBlobUrl && <>
+        <h2>Wavesurfer component</h2>
      <WaveSurfer onMount={handleWSMount}>
                         <WaveForm waveColor={'f4f2f2'} cursorColor={'#00000000'} progressColor={'#EA1073'} height={50}
                                   id={`waveform`} fillParent={false} responsive={true}
@@ -48,6 +66,20 @@ function App() {
                         </WaveForm>
                     </WaveSurfer>
       <button onClick={play}>play/pause</button>
+      <h2>
+        Html audio component
+      </h2>
+      <audio controls>
+        <source src={mediaBlobUrl}/>
+      </audio>
+        </>
+
+      }
+      <p>{status}</p>
+      <button onClick={handleToggleRecoring}>
+        {status === 'recording' ? 'Stop':'Record'}
+      </button>
+
     </div>
   );
 }
